96825419044b2fdb8e52f3e0243d89dedf68fcac
sohanrajannavar3646-netizen <sohanrajannavar3646@gmail.com>
2026-04-14 20:40:14 +0530

    Update client.py

--- client.py
+++ client.py
@@ -10,32 +10,17 @@ context.check_hostname = False
 context.verify_mode = ssl.CERT_NONE
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client = context.wrap_socket(client, server_hostname='localhost')
-client.connect((HOST, PORT))
+secure_client = context.wrap_socket(client, server_hostname=HOST)
+secure_client.connect((HOST, PORT))
 
 username = input("Enter username: ")
-client.send(username.encode())
+secure_client.send(username.encode())
 
 def receive():
     while True:
         try:
-            message = client.recv(1024).decode()
-
-            if message.startswith("[File]"):
-                parts = message.split(" ", 2)
-                sender = parts[1]
-                filename = parts[2]
-
-                data = client.recv(4096)
-
-                with open("received_" + filename, "wb") as f:
-                    f.write(data)
-
-                print(f"Received file from {sender}: {filename}")
-
-            else:
-                print(message)
-
+            message = secure_client.recv(1024).decode()
+            print(message)
         except:
             break
 
@@ -44,24 +29,13 @@ def write():
         message = input()
 
         if message.startswith("/file"):
-            filename = message.split(" ", 1)[1]
-
-            try:
-                with open(filename, "rb") as f:
-                    data = f.read()
-
-                client.send(message.encode())
-                client.send(data)
-
-            except:
-                print("File not found")
+            filename = message.split()[1]
+            with open(filename, "r") as f:
+                data = f.read()
+            secure_client.send(f"/file {filename} {data}".encode())
 
         else:
-            client.send(message.encode())
-
-            if message == "/quit":
-                client.close()
-                break
+            secure_client.send(message.encode())
 
 threading.Thread(target=receive).start()
 threading.Thread(target=write).start()
